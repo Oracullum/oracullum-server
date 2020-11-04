febbe4a449ededd6ff3973419936e71c181ef89e
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-04 20:14:21 -0300

    feat: implementing the password with cryptography

--- src/app/controllers/UsersController.ts
+++ src/app/controllers/UsersController.ts
@@ -1,14 +1,15 @@
 import User from "../models/User";
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import { hash } from 'bcryptjs';
 
 class UsersController{
   public async create(request: Request, response: Response){
     const { name, email, password } = request.body;
     
     const usersRepository = getRepository(User)
-
-    const user = await usersRepository.create({ name, email, password_hash: password })
+    const password_hash = await hash(password, 8);
+    const user = await usersRepository.create({ name, email, password_hash })
 
     await usersRepository.save(user);
 
