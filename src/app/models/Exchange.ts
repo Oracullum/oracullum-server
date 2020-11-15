d54ee72452e549d099de9a58cd85b818bdaec455
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-15 19:54:19 -0300

    refactor: refactoring the code

--- src/app/models/Exchange.ts
+++ src/app/models/Exchange.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Double, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import Stock from "./Stock";
 
 
