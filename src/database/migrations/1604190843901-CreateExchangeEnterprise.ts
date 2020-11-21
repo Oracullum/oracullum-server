3c62f505e50b119823c663dc0d23566db4c2263f
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-11-21 20:45:02 -0300

    feat: developing the migration Exchange Enterprise

---
+++ src/database/migrations/1604190843901-CreateExchangeEnterprise.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class CreateExchangeEnterprise1606000022077
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "exchanges_enterprises",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "enterprise_id",
+            type: "uuid",
+            isNullable: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "Enterprise_FK",
+            columnNames: ["enterprise_id"],
+            referencedTableName: "enterprises",
+            referencedColumnNames: ["id"],
+            onUpdate: "CASCADE",
+            onDelete: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("exchanges_enterprises");
+  }
+}
